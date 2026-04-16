8929e5ad4ba95539b881e9d642886a44ae1c9a6b
schoolMng1 <schoolmanagemnt818@gmail.com>
2026-04-16 15:44:19 +0530

    intial setup of auth service

---
+++ src/main/java/com/school_mng_auth/SchoolManagementAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.school_mng_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school_mng_auth/SchoolManagementAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school_mng_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
